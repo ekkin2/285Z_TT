0969d9545c98669dbe10082396b5227dc2206420
dakotadvanced <46972988+dakotadvanced@users.noreply.github.com>
2019-10-26 21:13:47 -0500

    bruh moment

--- src/285Z-Main/initRobot.cpp
+++ src/285Z-Main/initRobot.cpp
@@ -5,8 +5,8 @@
 //could be incorrect
 Motor frontLeft(11);
 Motor backLeft(12);
-Motor frontRight(13);
-Motor backRight(14);
+Motor frontRight(-13);
+Motor backRight(-14);
 
 //motors for the intake. (looking from the front)
 Motor intakeRight(16);
@@ -15,6 +15,15 @@ Motor intakeLeft(19);
 //solo angler motor
 Motor anglerCenter(20);
 
+okapi::ChassisScales scales{4_in, 11.5_in};
+
+std::shared_ptr<okapi::OdomChassisController> chassis = okapi::ChassisControllerBuilder()
+      .withMotors({ 11, 12 }, { -13, 14 })
+      .withGearset(okapi::AbstractMotor::gearset::green)
+      .withDimensions(scales)
+      .withOdometry(okapi::StateMode::FRAME_TRANSFORMATION, 0_mm, 0_deg, 0.0001_mps)
+      .buildOdometry();
+std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
 auto driveTrain = ChassisControllerFactory::create(
   -11, 1,
   AbstractMotor::gearset::green,
